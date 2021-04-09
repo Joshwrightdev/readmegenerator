1c704cf8d37d76743ddcb76669fab3c40228bd7e
Josh Wright <joshwrightaudio@gmail.com>
2021-04-08 22:27:24 -0400

    ..

--- index.js
+++ index.js
@@ -2,7 +2,27 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 // TODO: Create an array of questions for user input
-const questions = [];
+inquirer.prompt ([
+    
+])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
