dd42a05dcc65fc4c9651148ce9290afda11d5bdf
Josh Wright <joshwrightaudio@gmail.com>
2021-04-09 12:40:15 -0400

    functional

--- index.js
+++ index.js
@@ -1,38 +1,44 @@
-// TODO: Include packages needed for this application
 const inquirer = require("inquirer");
 const fs = require("fs");
-// TODO: Create an array of questions for user input
-inquirer.prompt([
-  {
-    type: "input",
-    name: "username",
-    message: "Enter your github Username:",
-  },
-  {
-    type: "input",
-    name: "title",
-    message: "Enter your project title :",
-  },
-  {
-    type: "input",
-    name: "email",
-    message: "Enter your Email:",
-  },
-  {
-    type: "input",
-    name: "description",
-    message: "Enter project description/details:",
-  },
-]);
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const generateMD = (answers) =>
+  ` ${answers.username} ${answers.title}${answers.email} ${answers.description}
+`;
+
+// TODO: Create an array of questions for user input
+inquirer
+  .prompt([
+    {
+      type: "input",
+      name: "username",
+      message: "Enter your github Username:",
+    },
+    {
+      type: "input",
+      name: "title",
+      message: "Enter your project title :",
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "Enter your Email:",
+    },
+    {
+      type: "input",
+      name: "description",
+      message: "Enter project description/details:",
+    },
+  ])
+  .then((answers) => {
+    const mdPageContent = generateMD(answers);
+
+    fs.writeFile("readme.md", mdPageContent, (err) =>
+      err ? console.log(err) : console.log("Successfully created ReadMe File!")
+    );
+  });
 
 // TODO: Create a function to initialize app
-function init() {}
+// function init() {}
 
-// Function call to initialize app
-init();
-
-
-
+// // Function call to initialize app
+// init();
